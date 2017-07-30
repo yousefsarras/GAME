ecf0f552e80f12fd11621574cc635bd58e8928ab
zjeffreyl <zjeffreyl@gmail.com>
2017-07-30 00:15:38 -0700

    Testing if saved

---
+++ GAMEUNITY/Assets/MinionAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionAI : MonoBehaviour {
+
+    public Transform target;
+    public float movespeed = 3;
+    Vector3 myPosition;
+
+	void Start () {
+        myPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetDirection = target.position - this.myPosition;
+        myPosition.x += targetDirection.x * movespeed * Time.deltaTime;
+        //Debug.Log("My minion position " + myPosition.x);
+	}
+}
