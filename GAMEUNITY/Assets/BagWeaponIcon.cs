be5ec5ed8e4bf739d1dc140e4377ad917095f598
DESKTOP-46NOMGO\Jeffrey Zhang <zjeffreyl@gmail.com>
2017-08-19 20:01:43 -0700

    sell button

---
+++ GAMEUNITY/Assets/BagWeaponIcon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagWeaponIcon : MonoBehaviour {
+
+    public bool itemSelected = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void onClick()
+    {
+        itemSelected = !itemSelected;
+        Debug.Log("Item Selected: " + itemSelected);
+    }
+	// Update is called once per frame
+	void Update () {
+		if(itemSelected == true &&)
+	}
+}
---
+++ GAMEUNITY/Assets/SellSwitch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SellSwitch : MonoBehaviour {
+
+    public bool sellSwitch;
+
+    public void Start()
+    {
+        sellSwitch = false;
+    }
+
+    public void OnClick()
+    {
+        sellSwitch = !sellSwitch;
+    }
+}
